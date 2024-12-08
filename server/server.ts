0fd6e5e9d574916de3a37b8467fc6b48634c94f4
jyothika28 <jyothikajalla032@gmail.com>
2024-12-08 01:33:24 -0500

    changed server imports and package.json

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import mongoose from "mongoose";
-import express, { type Express } from "express";
+import express, { type Express,Request,Response } from "express";
 import { Server } from "http"; // Import the Server type from Node.js
 import dotenv from "dotenv";
 import session from "express-session";
@@ -82,7 +82,7 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   console.log(req.session);
   res.send("REST Service for Fake SO");
   res.end();
